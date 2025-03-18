f4195d8e4590c224d7e7032a27811d21400e310a
27Fer12 <fr9780547@gmail.com>
2025-03-18 15:02:03 -0600

    Subiendo el proyecto fundamentosVue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,72 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="text-center">MASCOTAS</h1>
+    
+    <!-- Inicio del primer row -->
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header">
+            <h6>Gatos</h6>
+          </div>
+          <div class="card-body"></div>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header">
+            <h6>Perros</h6>
+          </div>
+          <div class="card-body"></div>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+    <!-- Cierre del primer row -->
+    
+    <!-- Inicio del segundo row -->
+    <div class="row mt-4">
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img src="https://assets.megamediaradios.fm/2015/08/gato5.gif" class="card-img-top" alt="Gato animado">
+          <div class="card-body text-center">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button class="btn-warning">gatitos</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img src="https://assets.megamediaradios.fm/2015/08/gato5.gif" class="card-img-top" alt="Gato animado">
+          <div class="card-body text-center">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button class="btn-warning">gatitos</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img src="https://assets.megamediaradios.fm/2015/08/gato5.gif" class="card-img-top" alt="Gato animado">
+          <div class="card-body text-center">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button class="btn-warning">gatitos</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Cierre del segundo row -->
+  </div>
+</template>
+
+<style scoped>
+/* Estilos específicos para este componente */
+.container {
+  padding: 20px;
+}
+</style>
