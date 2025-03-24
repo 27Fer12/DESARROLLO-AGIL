05746d6a680387fb414dd0c9f930bcda5aa7418d
Anamichel-leon <anamichel280503@gmail.com>
2025-03-24 13:37:37 -0600

    pagina web1

---
+++ src/components/Principall.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+import Card from './Card.vue';
+import carrusel from './carrusel.vue';
+import diseño from './diseño.vue';
+
+
+
+import Footer from './Footer.vue'; 
+import Navbar from './Navbar.vue';
+</script>
+
+<template>
+
+  <div class="container">
+    <diseño />
+
+    
+    <Navbar/>
+    
+
+    
+    <carrusel/>
+    
+
+    <!-- Primer row -->
+    <Card/>
+    
+    <!-- Footer -->
+    <Footer/>
+
+  </div>
+</template>
+
+<style scoped>
+/* Agrega tus estilos personalizados aquí */
+</style>
